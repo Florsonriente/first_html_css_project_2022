38fe400f74ad832669ab98fb3272165e828c15b6
Andrii Kalko <andrii.kalko@casavi.de>
2024-10-31 18:15:24 +0100

    Initial commit

---
+++ neue_seite/js/script.js
@@ -0,0 +1,47 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+
+
+
+
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+
+
+
+
+// alle Anker(Navigationslinks,Mouse-Anker) sollen Ihre lokale Verweise animiert ansteuern und mit einen Vesatz von 70px von oben unterhalt des fixierten headers positionieren.
+$('nav ul li a,.mouse,#scroll-to-top,.logo', ).click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+
